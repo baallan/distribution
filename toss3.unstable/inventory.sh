f301ea18f232eecb8df1c3440cf6560529aac808
root <root@twain.sandia.gov>
2018-04-13 13:18:44 -0600

    update to 3.4.6

--- toss3.unstable/inventory.sh
+++ toss3.unstable/inventory.sh
@@ -1,4 +1,5 @@
 # this dumps the file lists and dependency lists
 # for the generated rpms
-for i in ./ovis/Tars/ovis-3.4.0/toss3.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > plist
-for i in ./ovis/Tars/ovis-3.4.0/toss3.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > rlist
+ver=3.4.6
+for i in ./ovis/Tars/ovis-$ver/toss3.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > plist
+for i in ./ovis/Tars/ovis-$ver/toss3.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > rlist
