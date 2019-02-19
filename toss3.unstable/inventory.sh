b635fedeaabd9b97cb5d2c27627f15ba0b776946
Ben Allan <baallan@sandia.gov>
2019-02-19 10:22:33 -0700

    v4 test2

--- toss3.unstable/inventory.sh
+++ toss3.unstable/inventory.sh
@@ -1,5 +1,5 @@
 # this dumps the file lists and dependency lists
 # for the generated rpms
-ver=3.4.6
+ver=3.4.11
 for i in ./ovis/Tars/ovis-$ver/toss3.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > plist
 for i in ./ovis/Tars/ovis-$ver/toss3.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > rlist
