8a5e0e6b851d8a87cc21d98e3ea2d2d6f5aa81e9
Ben Allan <baallan@sandia.gov>
2019-03-01 14:03:33 -0700

    workable build on arm

--- v41.mayer.unstable/inventory.sh
+++ v41.mayer.unstable/inventory.sh
@@ -1,4 +1,4 @@
 # this dumps the file lists and dependency lists
 # for the generated rpms
-for i in ./ovis/Tars/ovis-4.*/v41.mayer.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > plist
-for i in ./ovis/Tars/ovis-4.*/v41.mayer.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > rlist
+for i in ./ovis/Tars/ovis-4.*/v41.mayer.unstable/RPMS/aarch64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > plist
+for i in ./ovis/Tars/ovis-4.*/v41.mayer.unstable/RPMS/aarch64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > rlist
