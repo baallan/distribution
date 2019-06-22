bdef86052f038fda5c0a8efb0ea4ee8e2e43e402
Ben Allan <baallan@sandia.gov>
2019-06-21 19:31:28 -0600

    toss3 v42 ovis build

--- v42.toss3.unstable/inventory.sh
+++ v42.toss3.unstable/inventory.sh
@@ -1,6 +1,7 @@
 # this dumps the file lists and dependency lists
 # for the generated rpms
-for i in ./sos/Tars/sosdb-4.*/v41.rhel7.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > sosdeps
-for i in ./sos/Tars/sosdb-4.*/v41.rhel7.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > sosfiles
-for i in ./ovis/Tars/ovis-4.*/v41.rhel7.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > ovisdeps
-for i in ./ovis/Tars/ovis-4.*/v41.rhel7.unstable/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > ovisfiles
+x=$(basename $(pwd))
+for i in ./sos/Tars/sosdb-4.*/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > sosdeps
+for i in ./sos/Tars/sosdb-4.*/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > sosfiles
+for i in ./ovis/Tars/ovis-4.*/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > ovisdeps
+for i in ./ovis/Tars/ovis-4.*/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > ovisfiles
