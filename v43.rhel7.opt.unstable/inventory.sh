cfc13fca9c30cdb8a8e152bb93039735e38657ce
Ben Allan <baallan@sandia.gov>
2019-12-06 15:43:51 -0700

    working software collection version, updated to 4.3.3 release

--- v43.rhel7.opt.unstable/inventory.sh
+++ v43.rhel7.opt.unstable/inventory.sh
@@ -3,5 +3,10 @@
 x=$(basename $(pwd))
 for i in ./sos/Tars/sosdb-4.*/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > sosdeps
 for i in ./sos/Tars/sosdb-4.*/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > sosfiles
-for i in ./ovis/Tars/ovis-4.*/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > ovisdeps
-for i in ./ovis/Tars/ovis-4.*/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > ovisfiles
+
+for i in ./ovis/Tars/ovis-4.*/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > ldmsdeps
+for i in ./ovis/Tars/ovis-4.*/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > ldmsfiles
+
+for i in ./collection/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qp --requires $i; echo; done > ovisdeps
+for i in ./collection/$x/RPMS/x86_64/*rpm; do echo $i; echo $i; rpm -qpl $i; echo; done > ovisfiles
+
